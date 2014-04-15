0b5fd3f1f392beb1fc66f98be6017546579e0429
Andy Roberts <blackcadillax@gmail.com>
2014-04-15 13:46:51 -0400

    Add function to convert RGB to HSL

--- script.js
+++ script.js
@@ -213,6 +213,30 @@ function clearBoxes() {
   $boxes.css("background-color", "rgb(200, 200, 200)");
 }
 
+// Convert a RGB value to a HSL value
+function rgbToHsl(r, g, b) {
+  r /= 255, g /= 255, b /= 255;
+  var max = Math.max(r, g, b);
+  var min = Math.min(r, g, b);
+  var h, s, l = (max + min) / 2;
+  if (max === min) {
+    h = s = 0;
+  } else {
+    var d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    switch (max) {
+        case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+        case g: h = (b - r) / d + 2; break;
+        case b: h = (r - g) / d + 4; break;
+    }
+    h /= 6;
+  }
+  h = Math.round(h * 360);
+  s = Math.round(s * 100);
+  l = Math.round(l * 100);
+  return [h, s, l];
+}
+
 $(document).ready(function() {
   
   if (localStorage.length !== 0) {
